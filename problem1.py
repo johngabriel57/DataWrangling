70fee7f65c8e93f70cfde3974bc8db8e676e9b63
johngabriel57 <57347050+johngabriel57@users.noreply.github.com>
2019-11-27 16:11:03 +0800

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data1= {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'Math': [80, 95, 79]}
+data2= {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'Electronics': [85, 81, 83]}
+data3= {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'GEAS': [90, 79, 93]}
+data4= {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'ESAT': [93, 89, 88]}
+
+math= pd.DataFrame(data1, columns= ['Student', 'Math'])
+electronics= pd.DataFrame(data2, columns= ['Student', 'Electronics'])
+esat= pd.DataFrame(data4, columns= ['Student', 'ESAT'])
+geas= pd.DataFrame(data3, columns= ['Student', 'GEAS'])
+
+merge= pd.merge(math,electronics, on='Student')
+merge1= pd.merge(merge, esat, on='Student')
+bear_grades= pd.merge(merge1, geas, on='Student')
