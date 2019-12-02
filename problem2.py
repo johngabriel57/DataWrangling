1bff07e11b16cb91e0e4913110caad90b7e87f6a
johngabriel57 <57347050+johngabriel57@users.noreply.github.com>
2019-12-02 17:27:44 +0800

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = {'Box': ['Box1', 'Box1', 'Box1', 'Box2', 'Box2', 'Box2'], 'Dimension': ['Length', 'Width', 'Height',
+'Length', 'Width', 'Height'], 'Value': [6,4,2,5,3,4]}
+messy = pd.DataFrame(data, columns= ['Box','Dimension','Value'])
+tidy = messy.pivot_table(index=['Box'], columns='Dimension', values='Value')
+tidyfinal = messy.pivot_table(index=['Box'], columns='Dimension', values='Value').reset_index()
+tidyfinal['Volume'] = tidyfinal.Height*tidyfinal.Length*tidyfinal.Width
